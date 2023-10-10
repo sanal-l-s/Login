23e7989f6710922a9d6a88465b8140a9ff88d32a
Sanal L S <sanallsofficial@gmail.com>
2023-10-10 13:03:13 +0530

    added error message in the page

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.login
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,18 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLogin :Button = findViewById(R.id.btnLogin)
-        val etUserName :EditText = findViewById(R.id.etUserName)
-        val etPassword :EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val etUserName: EditText = findViewById(R.id.etUserName)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val txtErrorMessage: TextView = findViewById(R.id.txtErrorMessage)
 
         btnLogin.setOnClickListener {
             val username = etUserName.text.toString()
             val password = etPassword.text.toString()
 
             if (username == "admin" && password == "admin@123") {
+                txtErrorMessage.visibility = View.INVISIBLE
                 Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                txtErrorMessage.text = buildString {
+                    append("Invalid username or password")
+                }
+                txtErrorMessage.visibility = View.VISIBLE
+//                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
             }
         }
 
