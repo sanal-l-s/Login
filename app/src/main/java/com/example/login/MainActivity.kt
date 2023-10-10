70e4a97715f228bded94934000cd7181551f2ae3
Sanal L S <sanallsofficial@gmail.com>
2023-10-10 15:37:28 +0530

    added a profile card screen on successful login

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.login
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,22 +22,42 @@ class MainActivity : AppCompatActivity() {
         val etUserName: EditText = findViewById(R.id.etUserName)
         val etPassword: EditText = findViewById(R.id.etPassword)
         val txtErrorMessage: TextView = findViewById(R.id.txtErrorMessage)
+        val txtProfileName: TextView = findViewById(R.id.txtProfileName)
+        val llProfileScreen: LinearLayout = findViewById(R.id.llProfileScreen)
+        val llLoginScreen: LinearLayout = findViewById(R.id.llLoginScreen)
+
 
         btnLogin.setOnClickListener {
+
+            hideKeyboard()
+
             val username = etUserName.text.toString()
             val password = etPassword.text.toString()
 
             if (username == "admin" && password == "admin@123") {
                 txtErrorMessage.visibility = View.INVISIBLE
+                txtProfileName.text = username
+                llLoginScreen.visibility = View.GONE
+                llProfileScreen.visibility = View.VISIBLE
                 Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
             } else {
                 txtErrorMessage.text = buildString {
                     append("Invalid username or password")
                 }
                 txtErrorMessage.visibility = View.VISIBLE
+                llProfileScreen.visibility = View.GONE
+                llLoginScreen.visibility = View.VISIBLE
 //                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+
+    fun hideKeyboard() {
+        val view: View? = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
