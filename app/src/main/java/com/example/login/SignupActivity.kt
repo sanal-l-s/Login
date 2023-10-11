93df0cd6e1cb3ad5e1688ee9d7ea181a2816c975
Sanal L S <sanallsofficial@gmail.com>
2023-10-11 15:03:44 +0530

    storing userdata in arraylist while registering

--- app/src/main/java/com/example/login/SignupActivity.kt
+++ app/src/main/java/com/example/login/SignupActivity.kt
@@ -1,11 +1,58 @@
 package com.example.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        //region Init Views
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etUsername = findViewById<EditText>(R.id.etUserName)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val txtStatusMessage = findViewById<TextView>(R.id.txtStatusMessage)
+        //endregion
+
+        val userList= ArrayList<UserData>()
+
+
+
+        btnRegister.setOnClickListener {
+            val email = etEmail.text.toString()
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+
+            if (email.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty()) {
+                txtStatusMessage.visibility = View.GONE
+
+
+                userList.add(UserData(1, email, username, password))
+
+
+                txtStatusMessage.text = "Added details of $username"
+                txtStatusMessage.visibility = View.VISIBLE
+
+                if (userList.isNotEmpty()) {
+                    Log.d("LIST", "LIST: $userList")
+                }
+            } else {
+                txtStatusMessage.text = "Please enter the details"
+                txtStatusMessage.visibility = View.VISIBLE
+            }
+
+
+        }
+
+
+
     }
 }
