3f98533bc20faf2d9462a7de95135893b8c3bbe4
Sanal L S <sanallsofficial@gmail.com>
2023-10-11 12:38:39 +0530

    routed to profile activity on successful login

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.login
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -21,10 +22,9 @@ class MainActivity : AppCompatActivity() {
         val etUserName: EditText = findViewById(R.id.etUserName)
         val etPassword: EditText = findViewById(R.id.etPassword)
         val txtErrorMessage: TextView = findViewById(R.id.txtErrorMessage)
-        val txtProfileName: TextView = findViewById(R.id.txtProfileName)
-        val llProfileScreen: LinearLayout = findViewById(R.id.llProfileScreen)
-        val llLoginScreen: LinearLayout = findViewById(R.id.llLoginScreen)
-        val imgClose: ImageView = findViewById(R.id.imgClose)
+//        val txtProfileName: TextView = findViewById(R.id.txtProfileName)
+//        val llProfileScreen: LinearLayout = findViewById(R.id.llProfileScreen)
+//        val llLoginScreen: LinearLayout = findViewById(R.id.llLoginScreen)
 
 
         btnLogin.setOnClickListener {
@@ -36,23 +36,16 @@ class MainActivity : AppCompatActivity() {
 
             if (username == "admin" && password == "admin@123") {
                 txtErrorMessage.visibility = View.INVISIBLE
-                txtProfileName.text = username
-                llLoginScreen.visibility = View.GONE
-                llProfileScreen.visibility = View.VISIBLE
                 Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+
+                startActivity(Intent(this, ProfileActivity::class.java))
             } else {
                 txtErrorMessage.text = buildString {
                     append("Invalid username or password")
                 }
                 txtErrorMessage.visibility = View.VISIBLE
-                llProfileScreen.visibility = View.GONE
-                llLoginScreen.visibility = View.VISIBLE
 //                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
             }
-        }
-        imgClose.setOnClickListener {
-            llProfileScreen.visibility = View.GONE
-            llLoginScreen.visibility = View.VISIBLE
         }
 
     }
