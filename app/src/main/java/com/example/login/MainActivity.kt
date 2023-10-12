6384bb1cd4390a810a9b6c70975e4415f59e7c8a
Sanal L S <sanallsofficial@gmail.com>
2023-10-12 17:18:37 +0530

    cleaned code  by adding separate functions. added ?.let scope function. added extension function for error message

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,61 +3,68 @@ package com.example.login
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val btnLogin: Button by lazy { findViewById<Button>(R.id.btnLogin) }
+    private val etUserName: EditText by lazy { findViewById<EditText>(R.id.etUserName) }
+    private val etPassword: EditText by lazy { findViewById<EditText>(R.id.etPassword) }
+    private val txtErrorMessage: TextView by lazy { findViewById<TextView>(R.id.txtErrorMessage) }
+    private val txtSignup: TextView by lazy { findViewById<TextView>(R.id.txtSignup) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //region View Initialization
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        val etUserName: EditText = findViewById(R.id.etUserName)
-        val etPassword: EditText = findViewById(R.id.etPassword)
-        val txtErrorMessage: TextView = findViewById(R.id.txtErrorMessage)
-        val txtSignup: TextView = findViewById(R.id.txtSignup)
-        //endregion
+        setupClickListeners()
 
 
+    }
+
+    private fun setupClickListeners() {
         btnLogin.setOnClickListener {
-
             hideKeyboard()
 
             val username = etUserName.text.toString()
             val password = etPassword.text.toString()
 
-            if (username == "admin" && password == "admin@123") {
+            if (isCredentialsValid(username, password)) {
                 txtErrorMessage.visibility = View.INVISIBLE
                 Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
-
                 startActivity(Intent(this, ProfileActivity::class.java))
             } else {
-                txtErrorMessage.text = buildString {
-                    append("Invalid username or password")
-                }
-                txtErrorMessage.visibility = View.VISIBLE
-//                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                txtErrorMessage.showError("Invalid username or password")
             }
         }
+
         txtSignup.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
         }
+    }
 
+    private fun isCredentialsValid(username: String, password: String): Boolean {
+        return username == "admin" && password == "admin@123"
+    }
+
+    private fun TextView.showError(message: String) {
+        text = message
+        visibility = View.VISIBLE
     }
 
     private fun hideKeyboard() {
         val view: View? = this.currentFocus
-        if (view != null) {
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-        }
+        view?.let { hideSoftInputFromWindow(it.windowToken) }
+    }
+
+    private fun hideSoftInputFromWindow(windowToken: IBinder) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
