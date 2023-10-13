653547306bb24aba729d9144cb2f84f1cca5b359
Sanal L S <sanallsofficial@gmail.com>
2023-10-13 16:54:31 +0530

    added two fragments in profile page

--- app/src/main/java/com/example/login/ProfileActivity.kt
+++ app/src/main/java/com/example/login/ProfileActivity.kt
@@ -1,11 +1,28 @@
 package com.example.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
+    private val btnFrag1: Button by lazy { findViewById<Button>(R.id.btnFrag1) }
+    private val btnFrag2: Button by lazy { findViewById<Button>(R.id.btnFrag2) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        btnFrag1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, FirstFragment())
+                commit()
+            }
+        }
+        btnFrag2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, SecondFragment())
+                commit()
+            }
+        }
     }
 }
