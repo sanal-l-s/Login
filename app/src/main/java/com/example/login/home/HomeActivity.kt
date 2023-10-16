efdaf5251f660eb666d195c96468a8d46676311e
Sanal L S <sanallsofficial@gmail.com>
2023-10-16 15:23:48 +0530

    added fragments for bottom navigation view

--- app/src/main/java/com/example/login/home/HomeActivity.kt
+++ app/src/main/java/com/example/login/home/HomeActivity.kt
@@ -1,12 +1,52 @@
 package com.example.login.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.login.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private val homeBottomNav: BottomNavigationView by lazy { findViewById<BottomNavigationView>(R.id.navHomePage) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.containerHomeFragment, HomeFragment())
+            commit()
+        }
+
+        homeBottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navHome -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.containerHomeFragment, HomeFragment())
+                        commit()
+                    }
+                    true
+                }
+
+                R.id.navDashboard -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.containerHomeFragment, DashBoardFragment())
+                        commit()
+                    }
+                    true
+                }
+
+                R.id.navProfile -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.containerHomeFragment, ProfileFragment())
+                        commit()
+                    }
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+
     }
 }
