8048895a7eb10d69c52953e4c52a4cdecd3d4b2e
Sanal L S <sanallsofficial@gmail.com>
2023-10-18 18:01:08 +0530

    used parcelable plugin

--- app/src/main/java/com/example/login/data/Result.kt
+++ app/src/main/java/com/example/login/data/Result.kt
@@ -1,10 +1,11 @@
 package com.example.login.data
 
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Result(
     @SerializedName("Country")
     val country: String? = null,
@@ -16,32 +17,4 @@ data class Result(
     val mfrName: String? = null,
     @SerializedName("VehicleTypes")
     val vehicleTypes: List<VehicleType?>? = null
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(country)
-        parcel.writeString(mfrCommonName)
-        parcel.writeValue(mfrID)
-        parcel.writeString(mfrName)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Result> {
-        override fun createFromParcel(parcel: Parcel): Result {
-            return Result(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Result?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/com/example/login/data/VehicleType.kt
+++ app/src/main/java/com/example/login/data/VehicleType.kt
@@ -1,11 +1,14 @@
 package com.example.login.data
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class VehicleType(
     @SerializedName("IsPrimary")
     val isPrimary: Boolean? = null,
     @SerializedName("Name")
     val name: String? = null
-)
+) : Parcelable
